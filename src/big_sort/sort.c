9e7db867bac0800edf1947a82d3b691db97899cb
Busedame <147276221+Busedame@users.noreply.github.com>
2024-02-23 19:01:53 +0100

    Update sort.c

--- src/big_sort/sort.c
+++ src/big_sort/sort.c
@@ -14,35 +14,28 @@
 
 static void	ft_to_b(t_list **a, t_list **b, int len)
 {
-	t_list	*curr_a;
-	t_list	*curr_b;
+	t_list	*cheapest;
 	int		lowestcost;
 
 	while (len > 3)
 	{
-		curr_a = *a;
-		curr_b = *b;
 		ft_target_node_in_b(a, b);
 		ft_pushcost_to_b(a, b);
 		lowestcost = ft_what_is_lowest_cost(a);
-		curr_a = ft_where_is_lowest_cost(a, lowestcost);
-		ft_push_to_b(a, b, curr_a);
+		cheapest = ft_where_is_lowest_cost(a, lowestcost);
+		ft_push_to_b(a, b, cheapest);
 		len--;
 	}
 }
 
 static void	ft_to_a(t_list **a, t_list **b)
 {
-	t_list	*curr_a;
 	t_list	*curr_b;
 	int		lowestcost;
-	int		check;
 
 	curr_b = *b;
-	check = 0;
 	while (curr_b)
 	{
-		curr_a = *a;
 		ft_target_node_in_a(a, b);
 		ft_pushcost_to_a(a, b);
 		lowestcost = ft_what_is_lowest_cost(b);
